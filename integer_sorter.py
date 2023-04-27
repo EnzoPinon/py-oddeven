28c54cdb82f22751d408a65b40e33f776f53b2f9
EnzoPinon <mrlorenzopinon@gmail.com>
2023-04-28 01:06:22 +0800

    add a starting msg so we can simply append

--- integer_sorter.py
+++ integer_sorter.py
@@ -4,4 +4,10 @@ def process():
     initial_write_1 = open("even.txt", 'w')
     initial_write_2 = open("odd.txt", 'w')
 
+    initial_write_1.write("All numbers here are even.\n")
+    initial_write_2.write("All numbers here are odd.\n")
+
+    initial_write_2.close()
+    initial_write_1.close()
+
 process()
