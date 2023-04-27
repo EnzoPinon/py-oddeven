60a700a0781b6b8438e7cb78a2875740493b7fa6
EnzoPinon <mrlorenzopinon@gmail.com>
2023-04-28 01:07:51 +0800

    simply, modulo with a for loop

--- integer_sorter.py
+++ integer_sorter.py
@@ -9,5 +9,16 @@ def process():
 
     initial_write_2.close()
     initial_write_1.close()
-
+    with open("integers.txt", 'r') as integers, open("odd.txt", 'a') as odd, open("even.txt", 'a') as even:
+        #make a loop
+        for line in integers:
+            #convert to integer
+            number = int(line)
+            #divide with modulo 2
+            check = number % 2
+            #see if it returns a 0
+            if check == 0:
+                even.write(line)
+            if check != 0:
+                odd.write(line)
 process()
